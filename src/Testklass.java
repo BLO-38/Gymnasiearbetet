10142781cf05e58061d4f4fcd1ed3326193007fb
BLO-38 <lars3838@gmail.com>
2022-10-06 13:59:29 +0200

    Jag la till en utskrift

--- src/Testklass.java
+++ src/Testklass.java
@@ -1,5 +1,6 @@
 public class Testklass {
     public static void main(String[] args) {
         System.out.println("Detta är Lars som skriver");
+        System.out.println("Men nu är det Bosse!");
     }
 }
