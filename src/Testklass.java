d96825a682fa5ea2dbcdc200b8270cdfc89b1d7c
Lars Blomgren <53094710+BLO-38@users.noreply.github.com>
2022-10-06 17:22:30 +0200

    Update Testklass.java

--- src/Testklass.java
+++ src/Testklass.java
@@ -2,6 +2,8 @@ public class Testklass {
     public static void main(String[] args) {
         // Hej
         boolean b = false;
+        long loooong = 123;
+        // GITTEN
         System.out.println("Detta är Lasse som skriver");
         System.out.println("Men nu är det Bosse!");
         // Kodat i github
