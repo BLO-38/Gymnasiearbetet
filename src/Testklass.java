dd28cb8a2c7bd150df4b335ac0626fe9214c74d5
Lars Blomgren <53094710+BLO-38@users.noreply.github.com>
2022-10-06 17:19:22 +0200

    Update Testklass.java

--- src/Testklass.java
+++ src/Testklass.java
@@ -1,9 +1,10 @@
 public class Testklass {
     public static void main(String[] args) {
-        
+        sou
+            
         System.out.println("Detta är Lasse som skriver");
         System.out.println("Men nu är det Bosse!");
         // Kodat i github
-        // Testar rebase
+        // Testar rebase igen
     }
 }
