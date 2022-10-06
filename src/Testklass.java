2a7148ee729e663332f9ca211bce057a8faecd10
Lars Blomgren <53094710+BLO-38@users.noreply.github.com>
2022-10-06 17:14:54 +0200

    Testkommentar i github

--- src/Testklass.java
+++ src/Testklass.java
@@ -2,5 +2,6 @@ public class Testklass {
     public static void main(String[] args) {
         System.out.println("Detta är Lars som skriver");
         System.out.println("Men nu är det Bosse!");
+        // Kodat i github
     }
 }
